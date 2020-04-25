f8b3ca27a7f6add74349051432abdcfaa428e8f8
bmunnabe <35695944+bmunnabe@users.noreply.github.com>
2020-04-25 21:11:00 +0530

    Update fat-arrow-function.js

--- fat-arrow-function.js
+++ fat-arrow-function.js
@@ -13,7 +13,6 @@ function Munna() {
 obj = new Munna();
 obj.printAge()
 
-
 // To fix the above issue, We will store this object into some variable and use it as workaround(fix) like below
 
 function Munna() {
@@ -26,11 +25,9 @@ function Munna() {
 obj = new Munna();
 obj.printAge()
 
-
 // In some other scenarios like when pass function to other class or function. We will use bind, call, apply to bind this to that method.
 // But on Fat arrow function, this issue is fixed. So when you declare function as arrow function and call it.
 // It doen't from where it has been called or who called it. THIS on arrow function always refer to its parent (Class, function)
-
 
 function Munna() {
   this.age = 0;
