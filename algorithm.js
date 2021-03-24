d86764b677c6e38a4e0933eab8a21aae11a9d688
bmunnabe <35695944+bmunnabe@users.noreply.github.com>
2021-03-24 18:55:09 +0530

    Update algorithm.js

--- algorithm.js
+++ algorithm.js
@@ -10,3 +10,33 @@ const findMinimum = ( arr ) => {
 }
 
 // findMinimum( [ 2, 4, 5, 6, 7, 10, 1, 19, 39] ), Output : 1
+
+
+
+const sortingArray = ( arr ) => {
+  for ( let i = 0; i < arr.length; i++ ) {
+    let currentItem = arr[ i ];
+    for ( let j = i + 1; j < arr.length; j++ ) {
+      let nextItem = arr[ j ];
+      if ( currentItem > nextItem ) {
+        arr[ i ] = nextItem;
+        arr[ j ] = currentItem;
+        currentItem = arr[ i ];
+        nextItem = arr[ j ];
+      } 
+    }
+  }
+  return arr;
+}
+
+const sortingArray = ( arr ) => {
+  for ( let i = 0; i < arr.length; i++ ) {
+    for ( let j = i + 1; j < arr.length; j++ ) {
+      if ( arr[ i ] > arr[ j ] ) {
+        [ arr[ i ], arr[ j ] ] = [ arr[ j ], arr[ i ] ];
+      } 
+    }
+  }
+  return arr;
+}
+// console.log( sortingArr( [ 2, 4, 5, 1, 3, 5, 8 ]) ); , Output : [1, 2, 3, 4, 5, 5, 8]
