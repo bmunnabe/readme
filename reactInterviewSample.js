162f026fab6892847c0a49e25af5e867890ab68f
bmunnabe <35695944+bmunnabe@users.noreply.github.com>
2021-06-24 18:02:28 +0530

    Update reactInterviewSample.js

--- reactInterviewSample.js
+++ reactInterviewSample.js
@@ -33,3 +33,63 @@ const myHtmlContent = () => ( { __html: 'First &middot; Second' } )
 function MyComponent() {
   return <div dangerouslySetInnerHTML={myHtmlContent()} />
 }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  //HOOKS
+import React, { useEffect, useState } from "react";
+import "./style.css";
+
+export default function App() {
+
+  const [age, setAge] = useState(99)
+  const handleClick = () => setAge(age + 1)
+
+  useEffect( () => {
+    console.log( 'test' );
+    return () => {
+      console.log('called during unmounting');
+    }
+  }, [ age ] );
+
+  const someValue = "someValue"
+  const doSomething = () => {
+    return someValue
+  }
+
+  return (
+    <div>
+      <Age age={age} handleClick={handleClick}/>
+      <Instructions doSomething={doSomething} />
+    </div>
+  )
+}
+
+
+const Age = ({ age, handleClick }) => {
+return (
+  <div>
+    <div style={{ border: '2px', background: "papayawhip", padding: "1rem" }}>
+      Today I am {age} Years of Age
+    </div>
+    <pre> - click the button below 👇 </pre>
+    <button onClick={handleClick}>Get older! </button>
+  </div>
+)
+}
+
+const Instructions = React.memo((props) => {
+return (
+  <div style={{ background: 'black', color: 'yellow', padding: "1rem" }}>
+    <p>Follow the instructions above as closely as possible</p>
+  </div>
+)
+})
+
