a8bb6e90643c6ef1358fb6874ae5de2f995152aa
bmunnabe <35695944+bmunnabe@users.noreply.github.com>
2021-07-13 13:52:39 +0530

    Update algorithm.js

--- algorithm.js
+++ algorithm.js
@@ -1,3 +1,31 @@
+/*
+getMinimumReversalOfCoins:
+There are N coins, each showing either heads or tails. We would like all the coins to form a sequence of alternating heads and tails. What is the minimum number of coins that must be reversed to achieve this?
+Examples:
+
+1. Given array A = [1,0, 1, 0, 1, 1], the function should return 1. After reversing the sixth coin, we achieve an alternating sequence of coins [1, 0, 1, 0, 1, 0].
+2. Given array A = [1,1,0, 1, 1], the function should return 2. After reversing the first and fifth coins, we achieve an alternating sequence [0, 1, 0, 1, 0].
+3. Given array A = [0, 1, 0], the function should return O. The sequence of coins is already alternating.
+4. Given array A = [0, 1, 1, 0], the function should return 2. We can reverse the first and second coins to get the sequence: [1, 0, 1, 0].
+*/
+
+const getMinimumReversalOfCoins = ( list ) => {
+    const reverse = ( val ) => val === 0 ? 1 : 0;
+    const getReverse = ( arr, expected ) => {
+        let reversed = 0;
+        arr.forEach( ( item ) => {
+            if ( expected !== item ) {
+                reversed++;
+            }
+            expected = reverse( expected )
+        } );
+        return reversed;
+    };
+    return Math.min( getReverse( list, 0 ), getReverse( list, 1 ) );
+}
+
+
+
 const findMinimum = ( arr ) => {
   let currentMin;
   currentMin = arr[ 0 ];
